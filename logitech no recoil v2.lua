034d43cb33579c3bd58936a1dfcae4e7b3f261bc
rabbitfishy <144937211+rabbitfishy@users.noreply.github.com>
2025-01-03 11:45:00 +0000

    Update logitech no recoil v2.lua

--- logitech no recoil v2.lua
+++ logitech no recoil v2.lua
@@ -40,7 +40,7 @@ function NoRecoil()
       -- timer here is to limit how far we pull down the mouse.
       -- 1000 = 1 second.
       if PullTime < 8000 then
-        PullRate = 9 -- this is how much we have to pull down.
+        PullRate = 10 -- this is how much we have to pull down.
       end
       MoveMouseRelative(0, PullRate)
       Sleep(SleepAmount)
